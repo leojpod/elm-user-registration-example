f42b6ca3ec79a38e1f148bec436d5cd97ef52074
Leo jPod <leo.jpod@lilo.org>
2020-10-06 10:37:39 +0200

    start to work on the model

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (main)
 import Browser
 import Html exposing (div, span, text)
 import Html.Attributes exposing (class)
+import RemoteData exposing (WebData)
 
 
 main : Program Flags Model Msg
@@ -15,8 +16,75 @@ main =
         }
 
 
+type UserType
+    = CustomerUser
+    | VendorUser
+
+
+type User
+    = Customer_ Customer
+    | Vendor_ Vendor
+
+
+type alias Customer =
+    { email : String
+    , firstName : String
+    , lastName : String
+    }
+
+
+defaultCustomer : Customer
+defaultCustomer =
+    { email = ""
+    , firstName = ""
+    , lastName = ""
+    }
+
+
+type CustomerField
+    = CustomerEmail String
+    | FirstName String
+    | LastName String
+
+
+type alias Vendor =
+    { email : String
+    , companyName : String
+    , productName : String
+    , productPrice : Maybe Int
+    , productQuantity : Maybe Int
+    }
+
+
+defaultVendor : Vendor
+defaultVendor =
+    { email = ""
+    , companyName = ""
+    , productName = ""
+    , productPrice = Nothing
+    , productQuantity = Nothing
+    }
+
+
+type VendorField
+    = VendorEmail String
+    | CompanyName String
+    | ProductName String
+    | ProductPrice Int
+    | ProductQuantity Int
+
+
 type alias Model =
-    ()
+    { form : User
+    , request : WebData ()
+    }
+
+
+defaultModel : Model
+defaultModel =
+    { form = Customer_ defaultCustomer
+    , request = RemoteData.NotAsked
+    }
 
 
 type alias Flags =
@@ -25,18 +93,21 @@ type alias Flags =
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( (), Cmd.none )
+    ( defaultModel, Cmd.none )
 
 
 type Msg
     = NoOp
+    | SetUserType UserType
+    | SetCustomerField CustomerField
+    | SetVendorField VendorField
+    | Submit
+    | UserCreationUpdate (WebData ())
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
+    ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
